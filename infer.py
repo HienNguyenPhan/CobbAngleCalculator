f2688f256667fd06f3fefe24bd315d85fb85e67f
Nguyen Phan Hien <nphien08@gmail.com>
2025-08-03 23:49:45 +0700

    Final commit

--- infer.py
+++ infer.py
@@ -1,28 +1,24 @@
 import sys
-import cv2
 import torch
-import numpy as np
-from torchvision import transforms
-from PIL import Image
-from utils import extract_keypoints_from_heatmap, predict_cobb_from_image
-from model import get_model
-import matplotlib.pyplot as plt
+from utils import predict_cobb_from_image
+from model import Model
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def infer(IMAGE_DIR, CHECKPOINT_DIR):
-    model = get_model()
+    model = Model()
     model.to(device).eval()
     model.load_state_dict(torch.load(CHECKPOINT_DIR))
     print("Load state successful")
-    cobb_angle, keypoints = predict_cobb_from_image(IMAGE_DIR, model, device=device)
+    cobb_angle, _ = predict_cobb_from_image(IMAGE_DIR, model, device=device)
     return cobb_angle
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("The cobb angle is: <angle>")
+        print("Usage: python infer.py <image_path> <checkpoint_path>")
         sys.exit(1)
     
     image_dir = sys.argv[1]
     checkpoint_dir = sys.argv[2]
     angle = infer(image_dir, checkpoint_dir)
+    print(f"The Cobb angle is: {angle:.2f}")
